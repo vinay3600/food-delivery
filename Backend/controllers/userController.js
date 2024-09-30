913aca11524aad8a70c86e553bc6f4df05497da7
vinaykumar <kumarvinay30897@gmail.com>
2024-10-01 01:21:55 +0530

    fourth commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,7 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
 import validator from "validator"
-
+const bcrypt = require('bcryptjs');
 
 //login user 
 const loginUser = async (req,res) => {
